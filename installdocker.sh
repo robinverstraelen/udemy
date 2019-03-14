e9ede8d6e8b7e2cb5e1215060cb230e8bd5aa963
robinverstraelen <47820106+robinverstraelen@users.noreply.github.com>
2019-03-14 13:32:30 +0100

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -13,4 +13,4 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
